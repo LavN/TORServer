2ab4c69a5996a8500787aa9e67ed33d1647d60c9
LavN <ns.lavrenko@mail.ru>
2014-01-15 14:18:34 +0400

    add href on main page
    Добавлено расписание сессии

--- client/app/views/model/block/table/table.js
+++ client/app/views/model/block/table/table.js
@@ -4,12 +4,12 @@ window.TableBlockModelView = ModelView.extend({
   		var url = this.model.get("src");
   		if(url){
   			if(url.match(/\.pdf$/)) this.$('#source').append('<a href=\"'+url+'\">'+'<img class="source_icon" src="/static/img/icon_pdf.gif")></a>');
-  			if(url.match(/\.doc$/)) this.$('#source').append('<a href=\"'+url+'\">'+'<img class="source_icon" src="/static/img/icon_word.jpg")></a>');
+  			if(url.match(/\.doc$/)|| url.match(/\.docx$/)) this.$('#source').append('<a href=\"'+url+'\">'+'<img class="source_icon" src="/static/img/icon_word.jpg")></a>');
   			if(url.match(/\.zip$/)) this.$('#source').append('<a href=\"'+url+'\">'+'<img class="source_icon" src="/static/img/icon_wzip.gif")></a>');
 
   		}else(this.$("#source").hide());
-  		
-  		this.$("#table").html(this.model.get("html"));
+  		if(this.model.get("style") != undefined) this.$(".table-wrap").addClass(this.model.get("style"));
+  		this.$(".table-wrap").html(this.model.get("html"));
   		
   }
 });
