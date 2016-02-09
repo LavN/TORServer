1d9013fc685c540fbf8c9105ddd66c61368265ff
LavN <ns.lavrenko@mail.ru>
2016-02-09 14:52:11 +0300

    update subjects

--- client/app/views/action/sbj_ref/sbj_ref.js
+++ client/app/views/action/sbj_ref/sbj_ref.js
@@ -4,7 +4,7 @@ window.Sbj_refActionView = ActionView.extend({
   	var mdl = this.context;
   	this.$('a').attr("data-route","/edu/"+mdl.get("sgroup")+mdl.get("url"));
   	this.$('a').text(mdl.get("name"));
-  	if(mdl.get("view_class")=="light")this.$('li').css( "list-style-image",'url("/static/img/icon-no-info.jpg")');
+  	// if(mdl.get("view_class")=="light")this.$('li').css( "list-style-image",'url("/static/img/icon-no-info.jpg")');
   }
     
 });
